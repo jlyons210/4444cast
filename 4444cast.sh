28c81eddd859a154625639d86c316d671f581579
Jeremy Lyons <jlyons210@gmail.com>
2022-12-23 10:45:08 -0600

    Mostly working/finished - this is where I lose interest

--- 4444cast.sh
+++ 4444cast.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "" ]]; then
 
     ## Display usage if ZIP not provided
     echo "Usage:"
-    echo "  $0 zip_code"
+    echo "  $0 zip_code [limit]"
     exit 1
 
 else
@@ -24,6 +24,9 @@ else
 
 fi
 
+## Support 'limit' parameter
+if [[ "$2" != "" ]]; then LIMIT=$2; fi
+
 ## Get latitude and longitude by ZIP code
 LATLNG_JSON=$(./util_get_latlng_by_zip.sh $ZIP_CODE)
 LAT=$(jq -r ".lat" <<< $LATLNG_JSON)
@@ -40,5 +43,60 @@ NWS_FORECAST_API=$(jq -r ".properties.forecast" <<< $NWS_API_RESPONSE)
 
 ## Query NWS forecast endpoint
 NWS_FORECAST_API_RESPONSE=$(curl -s $NWS_FORECAST_API)
-PERIODS=$(jq -r '.["properties"].periods[]' <<< $NWS_FORECAST_API_RESPONSE)
-echo $PERIODS
+PERIODS=$(jq -r '.["properties"].periods' <<< $NWS_FORECAST_API_RESPONSE)
+PERIOD_COUNT=$(jq -r length <<< $PERIODS)
+
+
+## Limit output if specified
+if [ "$LIMIT" != "" ] && [ $PERIOD_COUNT -gt $LIMIT ]; then PERIOD_COUNT=$LIMIT; fi
+
+## Set constants
+DEG_SYMBOL=$'\xc2\xb0'
+ICON_CLOUDY=$'\xE2\x98\x81'
+ICON_MOSTLY_CLEAR=$'\xF0\x9F\x8C\x99'
+ICON_PARTLY_SUNNY=$'\xF0\x9F\x8C\xA4'
+ICON_RAIN=$'\xF0\x9F\x8C\xA7'
+ICON_SUNNY=$'\xF0\x9F\x8C\x9E'
+
+for (( i=1; i<=$PERIOD_COUNT; i++ ))
+do
+
+    ## Capture current conditions
+    PERIOD_NAME=$(jq -r ".[] | select(.number==$i).name" <<< $PERIODS)
+    PERIOD_FC_SHORT=$(jq -r ".[] | select(.number==$i).shortForecast" <<< $PERIODS)
+    PERIOD_TEMP=$(jq -r ".[] | select(.number==$i).temperature" <<< $PERIODS)
+    PERIOD_TEMP_UNIT=$(jq -r ".[] | select(.number==$i).temperatureUnit" <<< $PERIODS)
+
+    ## Set weather emojis
+    case $PERIOD_FC_SHORT in
+
+        "Mostly Clear")
+            PERIOD_FC_ICON=$ICON_MOSTLY_CLEAR
+            ;;
+
+        *"Cloudy"*)
+            PERIOD_FC_ICON=$ICON_CLOUDY
+            ;;
+
+        "Partly Sunny")
+            PERIOD_FC_ICON=$ICON_PARTLY_SUNNY
+            ;;
+
+        *"Rain"*)
+            PERIOD_FC_ICON=$ICON_RAIN
+            ;;
+
+        "Sunny")
+            PERIOD_FC_ICON=$ICON_SUNNY
+            ;;
+
+        *)
+            PERIOD_FC_ICON=""
+            ;;
+
+    esac
+
+    ## Report conditions
+    echo "$PERIOD_NAME: $PERIOD_FC_ICON $PERIOD_FC_SHORT $PERIOD_TEMP$DEG_SYMBOL$PERIOD_TEMP_UNIT"
+
+done
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 4444cast
 Playing around with geolocation and weather APIs
+
+## TODO:
+- [ ] Add local caching of ZIP/lat/long to greatly reduce Google Maps API calls
+- [ ] Re-write in Python, for great learning.
+- [ ] \(Stretch\) Re-write in Rust, for greater learning.
